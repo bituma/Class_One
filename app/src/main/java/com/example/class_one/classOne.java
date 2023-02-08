76194508eb9d0a882b51e03cf0093ce9b272f88f
bituma <104162170+bituma@users.noreply.github.com>
2023-02-08 17:57:24 +0000

    we just finished class one

---
+++ app/src/main/java/com/example/class_one/classOne.java
@@ -0,0 +1,15 @@
+package com.example.class_one;
+
+import android.util.Log;
+
+import java.io.PrintStream;
+
+public class classOne {
+    //If you have three java files a,b,c and two of them depends on c to run, which of the classes should be declared private or public?
+    //Ans:
+    //developer.android.com
+    String newString= "I am awesome";
+    String anotherString = " and  blessed";
+//    System.out.println("");
+//    PrintStream.
+}
